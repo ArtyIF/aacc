c75129a47d0413e61b2200d1815d7c6d915cfb10
ArtyIF <artyomisflash@mail.ru>
2024-10-28 18:59:22 +0300

    Added smoothing, so now AACC doesn't rely on FPS matching physics
    Also yes, I rebaked the lighting again, sorry

--- demo_assets/scripts/camera.gd
+++ demo_assets/scripts/camera.gd
@@ -1,15 +1,28 @@
 extends Camera3D
 @export var car: Car
-var _smoothed_direction: Vector3 = Vector3.FORWARD
+@export var follow_velocity: bool = false
+
+@onready var _follow_node: Node3D = car.get_node("Visuals")
+
+@onready var _last_position: Vector3 = _follow_node.global_position
+var _direction_target: Vector3 = -Vector3.FORWARD
+
+var _smoothed_direction: Vector3 = -Vector3.FORWARD
 var _smoothed_position: Vector3 = -Vector3.FORWARD
 
-func _ready() -> void:
-	pass # Replace with function body.
-
 # TODO: use _process and implement smoothing for cars
-func _physics_process(delta: float) -> void:
-	if car.ground_coefficient > 0:
-		_smoothed_direction = _smoothed_direction.slerp(Plane.PLANE_XZ.project(car.global_basis.z), delta * 10.0)
-	_smoothed_position = _smoothed_position.lerp(car.global_position + (_smoothed_direction * 4.0) + (Vector3.UP * 2.0), delta * 20.0)
+func _process(delta: float) -> void:
+	if follow_velocity:
+		var velocity: Vector3 = _last_position - _follow_node.global_position
+		if Plane.PLANE_XZ.project(velocity).length() >= delta:
+			_direction_target = Plane.PLANE_XZ.project(velocity.normalized())
+	else:
+		if car.ground_coefficient > 0.0:
+			_direction_target = Plane.PLANE_XZ.project(_follow_node.global_basis.z)
+
+	_smoothed_direction = _smoothed_direction.slerp(_direction_target, delta * 10.0)
+	_smoothed_position = _smoothed_position.lerp(_follow_node.global_position + (_smoothed_direction * 4.0) + (Vector3.UP * 2.0), delta * 20.0)
+
 	global_position = _smoothed_position
 	global_basis = Basis.looking_at(-_smoothed_direction)
+	_last_position = _follow_node.global_position
