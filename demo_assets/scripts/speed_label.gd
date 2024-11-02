d69a2ae75e438f04d1eb6a9ff3fe59fabb4348f1
ArtyIF <artyomisflash@mail.ru>
2024-11-02 14:50:29 +0300

    Who'd've known that the → symbol could tank performance so hard

--- demo_assets/scripts/speed_label.gd
+++ demo_assets/scripts/speed_label.gd
@@ -12,6 +12,6 @@ func get_gear_text(gear: int) -> String:
 func _physics_process(_delta: float) -> void:
 	var gear_text: String = get_gear_text(car.current_gear)
 	if car.switching_gears:
-		gear_text += " → " + get_gear_text(car.target_gear)
+		gear_text += " -> " + get_gear_text(car.target_gear)
 
 	text = "Gear %s\n%d km/h" % [gear_text, (car.linear_velocity.length() * 3.6)]
