1aa10dd352cb152f7ae8d035747fdc73138ea965
ArtyIF <artyomisflash@mail.ru>
2025-03-03 15:02:06 +0300

    Gears (kinda broken)

--- addons/aacc/scripts/car_plugins/engines/car_engine.gd
+++ addons/aacc/scripts/car_plugins/engines/car_engine.gd
@@ -1,14 +1,68 @@
 class_name CarEngine extends CarPluginBase
 
 @export var top_speed: float = 50.0
-@export var top_speed_reverse: float = 10.0
-@export var engine_force: float = 10000.0
+@export var max_engine_force: float = 10000.0
+@export var gears_count: int = 5
+@export var gear_switch_time: float = 0.1
+
+var current_gear: int = 0
+var target_gear: int = 0
+var switching_gears: bool = false
+var gear_switch_timer: float = 0.0
+var rpm_ratio: SmoothedFloat = SmoothedFloat.new()
 
 func _ready() -> void:
 	car.add_input("Accelerate")
 	car.add_input("Reverse")
 	car.add_input("Brake")
 	car.add_input("Handbrake")
+
+# TODO: rewrite this whole thing
+func update_gear(delta: float):
+	if target_gear != current_gear and not switching_gears and sign(current_gear) == sign(target_gear):
+		gear_switch_timer = gear_switch_time
+		switching_gears = true
+
+	if gear_switch_timer <= 0 or target_gear == current_gear:
+		current_gear = target_gear
+		switching_gears = false
+		
+	if is_zero_approx(car.get_param("GroundCoefficient")):
+		target_gear = current_gear
+		switching_gears = false
+
+	gear_switch_timer -= delta
+
+func get_gear_limit(gear: int) -> float:
+	return (1.0 / gears_count) * gear
+
+func set_current_gear():
+	var local_linear_velocity: Vector3 = car.get_param("LocalLinearVelocity")
+	
+	if (car.get_input("Handbrake") > 0.0 and local_linear_velocity.length() >= 0.25) or is_zero_approx(car.get_param("GroundCoefficient")):
+		return
+	
+	if car.get_param("VelocityZSign") > 0:
+		target_gear = -1
+		return
+
+	if abs(local_linear_velocity.z) < 0.25:
+		target_gear = -car.get_param("VelocityZSign")
+		return
+
+	var forward_speed_ratio: float = abs(local_linear_velocity.z / top_speed)
+	var lower_gear_limit_offset: float = (5.0 / 3.6) / top_speed # TODO: configure
+
+	if target_gear > 0 and forward_speed_ratio < get_gear_limit(target_gear - 1) - lower_gear_limit_offset:
+		target_gear -= 1
+	if forward_speed_ratio > get_gear_limit(target_gear) and target_gear < gears_count:
+		target_gear += 1
+
+# TODO: rpm
+
+func calculate_acceleration_multiplier() -> float:
+	var relative_speed: float = abs(car.get_param("LocalLinearVelocity").z) / (top_speed)
+	return clamp(1.0 - relative_speed, 0.0, 1.0)
 
 func process_plugin(delta: float) -> void:
 	if is_zero_approx(car.get_param("GroundCoefficient")):
@@ -23,9 +77,15 @@ func process_plugin(delta: float) -> void:
 	input_accelerate *= 1.0 - brake_value
 	input_reverse *= 1.0 - brake_value
 
+	set_current_gear()
+	update_gear(delta)
+
+	if switching_gears:
+		return
+
 	var force: Vector3 = Vector3.ZERO
 	if car.get_param("LocalLinearVelocity").z > -top_speed:
-		force += Vector3.FORWARD * input_accelerate * engine_force
-	if car.get_param("LocalLinearVelocity").z < top_speed_reverse:
-		force -= Vector3.FORWARD * input_reverse * engine_force
+		force += Vector3.FORWARD * input_accelerate * max_engine_force * calculate_acceleration_multiplier()
+	if car.get_param("LocalLinearVelocity").z < top_speed / gears_count:
+		force -= Vector3.FORWARD * input_reverse * max_engine_force * calculate_acceleration_multiplier()
 	car.add_force("Engine", force, true)
