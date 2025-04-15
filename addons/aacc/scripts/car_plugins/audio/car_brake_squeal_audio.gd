edc3ce3b27b641956a06445b0e83f323b652cb30
ArtyIF <artyomisflash@mail.ru>
2025-04-15 12:42:56 +0300

    Brake squeal

---
+++ addons/aacc/scripts/car_plugins/audio/car_brake_squeal_audio.gd
@@ -0,0 +1,23 @@
+class_name CarBrakeSquealAudio extends CarPluginBase
+@export var brake_audio_scene: PackedScene
+
+var player: AudioStreamPlayer3D
+var brake_volume: SmoothedFloat = SmoothedFloat.new(0.0, 10.0)
+
+func _ready() -> void:
+	player = brake_audio_scene.instantiate()
+	add_child(player)
+
+func process_plugin(delta: float) -> void:
+	var target_value: float = 0.0
+	if car.get_meta(&"gear_current", 0) != 0:
+		target_value = car.get_meta(&"input_brake", 0.0) * abs(car.get_meta(&"brake_speed", 0.0)) * car.get_meta(&"ground_coefficient", 0.0)
+	brake_volume.advance_to(clamp(target_value, 0.0, 1.0), delta)
+	player.volume_linear = brake_volume.get_value()
+
+	if car.freeze:
+		player.volume_linear = 0.0
+	if player.volume_db >= -60.0 and not player.playing:
+		player.play(randf_range(0.0, player.stream.get_length()))
+	elif player.volume_db < -60.0 and player.playing:
+		player.stop()
