f1fcafad42b91a3c7559245869987f67390b7782
ArtyIF <artyomisflash@mail.ru>
2025-06-03 01:17:00 +0300

    Separate handbrake force, boost and steer adjustments

--- addons/aacc/scripts/car_plugins/brakes/car_brake.gd
+++ addons/aacc/scripts/car_plugins/brakes/car_brake.gd
@@ -1,7 +1,8 @@
 class_name CarBrake extends CarPluginBase
 
 @export var max_brake_velocity: float = 0.1
-@export var brake_force: float = 20000.0
+@export var brake_force: float = 15000.0
+@export var handbrake_force: float = 20000.0
 
 func _ready() -> void:
 	car.set_meta(&"input_brake", 0.0)
@@ -13,12 +14,13 @@ func process_plugin(delta: float) -> void:
 
 	var input_brake: float = car.get_meta(&"input_brake", 0.0)
 	var input_handbrake: bool = car.get_meta(&"input_handbrake", false)
-	var brake_value: float = 1.0 if input_handbrake else input_brake
 
-	if is_zero_approx(brake_value):
+	if is_zero_approx(input_brake) and not input_handbrake:
 		return
 
 	var brake_speed: float = car.get_meta(&"local_linear_velocity", Vector3.ZERO).z / max_brake_velocity
 	car.set_meta(&"brake_speed", brake_speed)
-	var force: Vector3 = Vector3.FORWARD * brake_value * clamp(brake_speed, -1.0, 1.0) * brake_force
+	var force: Vector3 = Vector3.FORWARD * input_brake * clamp(brake_speed, -1.0, 1.0) * brake_force
+	if input_handbrake:
+		force += Vector3.FORWARD * clamp(brake_speed, -1.0, 1.0) * handbrake_force
 	car.set_force(&"brake", force, true)
--- addons/aacc/scripts/scene_plugins/inputs/car_input_boost.gd
+++ addons/aacc/scripts/scene_plugins/inputs/car_input_boost.gd
@@ -8,6 +8,6 @@ func _physics_process(delta: float) -> void:
 	if not car: return
 
 	var input_boost: bool = false
-	if car.linear_velocity.length() >= 0.25:
+	if car.linear_velocity.length() >= 0.25 or is_zero_approx(car.get_meta(&"ground_coefficient", 0.0)):
 		input_boost = Input.is_action_pressed(action_boost)
 	car.set_meta(&"input_boost", input_boost)
