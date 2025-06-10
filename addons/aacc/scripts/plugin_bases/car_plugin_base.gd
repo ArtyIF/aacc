6e9bcf73e3c2be36ea1867a8284e0c0e6bc4dda1
ArtyIF <artyomisflash@mail.ru>
2025-06-11 01:07:18 +0300

    Removed plugin deps, too cumbersome

--- addons/aacc/scripts/plugin_bases/car_plugin_base.gd
+++ addons/aacc/scripts/plugin_bases/car_plugin_base.gd
@@ -1,7 +1,7 @@
 abstract class_name CarPluginBase extends Node3D
 
 var car: Car
-var plugin_deps: Dictionary[StringName, CarPluginBase]
+
 var debuggable_properties: Array[StringName] = []
 
 func _enter_tree() -> void:
@@ -19,18 +19,6 @@ func _enter_tree() -> void:
 func _exit_tree() -> void:
 	if is_instance_valid(car):
 		car.remove_plugin(name)
-
-func update_plugin_deps(plugin_names: Array[StringName]) -> void:
-	plugin_deps.clear()
-	for plugin_name in plugin_names:
-		plugin_deps[plugin_name] = car.get_plugin(plugin_name)
-
-# key is the alias
-# value is the actual name
-func update_plugin_deps_aliased(plugin_names_map: Dictionary[StringName, StringName]) -> void:
-	plugin_deps.clear()
-	for plugin_name in plugin_names_map.keys():
-		plugin_deps[plugin_name] = car.get_plugin(plugin_names_map[plugin_name])
 
 func process_plugin(delta: float) -> void:
 	pass
