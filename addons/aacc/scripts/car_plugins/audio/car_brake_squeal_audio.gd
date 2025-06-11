4524262b935b1cd0d97c6f5c2433bbd84fea6424
ArtyIF <artyomisflash@mail.ru>
2025-06-11 15:05:35 +0300

    Fixed brake squeal

--- addons/aacc/scripts/car_plugins/audio/car_brake_squeal_audio.gd
+++ addons/aacc/scripts/car_plugins/audio/car_brake_squeal_audio.gd
@@ -5,6 +5,7 @@ var player: AudioStreamPlayer3D
 var brake_volume: SmoothedFloat = SmoothedFloat.new(0.0, 10.0)
 
 @onready var plugin_wp: CarWheelsProcessor = car.get_plugin(&"WheelsProcessor")
+@onready var plugin_brake: CarBrake = car.get_plugin(&"Brake")
 
 func _ready() -> void:
 	player = brake_audio_scene.instantiate()
@@ -13,7 +14,7 @@ func _ready() -> void:
 func process_plugin(delta: float) -> void:
 	var target_value: float = 0.0
 	if car.get_meta(&"gear_current", 0) != 0 and not car.get_meta(&"input_handbrake", false):
-		target_value = car.get_meta(&"input_brake", 0.0) * abs(car.get_meta(&"brake_speed", 0.0)) * plugin_wp.ground_coefficient
+		target_value = car.get_meta(&"input_brake", 0.0) * abs(plugin_brake.brake_speed) * plugin_wp.ground_coefficient
 	brake_volume.advance_to(clamp(target_value, 0.0, 1.0), delta)
 	player.volume_linear = brake_volume.get_value()
 
